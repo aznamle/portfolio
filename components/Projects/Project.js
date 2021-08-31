b1ba60903bfafe8f23ae4118f746123e84a532e9
aznamle <namle1998@hotmail.com>
2021-08-31 12:44:31 -0700

    created skills component, gitignore .env

--- components/Projects/Project.js
+++ components/Projects/Project.js
@@ -1,8 +1,6 @@
 import React, { useState, useMemo } from 'react'
 import ProjectModal from './ProjectModal'
 
-import useSWR from 'swr'
-
 const Project = ({ project }) => {
 
     const [open, setOpen] = useState(false)
@@ -16,10 +14,15 @@ const Project = ({ project }) => {
     return (
         <div className='mx-auto max-w-2xl grid grid-cols-2 gap-4 py-12 text-white'>
             {project.map(item => (
-                <div className='flex text-center mx-auto justify-center space-y-2 border-b pb-4 border-opacity-50' key={item.id}>
+                <div className='flex space-x-4 text-center mx-auto justify-center space-y-2 border-b pb-4 border-opacity-50' key={item.id}>
                     {item.title}
                     <div>
-                        <button className='bg-gray-700 border-white transition ease-in-out duration-300 hover:bg-gray-800 py-1 px-3 rounded-3xl' onClick={() => openProject(item)}>View</button>
+                        <button 
+                            className='bg-gray-700 border-white transition ease-in-out duration-300 hover:bg-gray-800 py-1 px-3 rounded-3xl' 
+                            onClick={() => openProject(item)}
+                        >
+                            View
+                        </button>
                     </div>
                 </div>
             ))}
@@ -30,6 +33,3 @@ const Project = ({ project }) => {
 }
 
 export default Project
-
-
-//align-bottom bg-black rounded-lg border border-gray-600 text-left overflow-hidden shadow-xl transform transition-all sm:my-24 sm:align-bottom max-h-4/5 overflow-y-auto sm:max-w-4xl sm:w-full
