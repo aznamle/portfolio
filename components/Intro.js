f2734707e9f9b84ff6025e1e6650e32bcd6a9a78
aznamle <namle1998@hotmail.com>
2021-08-31 10:22:58 -0700

    intro changes

--- components/Intro.js
+++ components/Intro.js
@@ -2,16 +2,20 @@ import React from 'react'
 import Image from 'next/image'
 
 const Intro = ({ about }) => {
+    console.log(about)
     return (
         <div className='flex container mx-auto max-w-3xl py-24 text-white'>
             <div className='w-1/3'>
-                {/* <Image className='rounded-full' src='/robot2.png' alt='profile image' width={200} height={200} /> */}
+                <Image className='rounded-full' src='/robot2.png' alt='profile image' width={250} height={250} />
             </div>
             
-            <div className='w-2/3'>
-            <p className=' leading-relaxed tracking-normal text-lg'>
-                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
+            <div className='w-2/3 ml-12'>
+                <h1 className='text-3xl'>
+                    {about.name}
+                </h1>
+                <p className=' leading-relaxed tracking-normal text-lg font-light'>
+                    {about.description}
+                </p>
             </div>
         </div>
     )
