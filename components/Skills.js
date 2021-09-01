3832f6b47eac820d7fab52ea78a0ef1925c2c951
aznamle <namle1998@hotmail.com>
2021-09-01 16:16:33 -0700

    content in skills component

--- components/Skills.js
+++ components/Skills.js
@@ -3,8 +3,15 @@ import React from 'react'
 const Skills = ({ skills }) => {
 
     return (
-        <div className='max-w-5xl mx-auto py-4'>
-            <h1 className='text-gray-100 text-3xl font-bold'>Skills</h1>            
+        <div className='max-w-5xl mx-auto py-4 space-y-4'>
+            <h1 className='text-gray-100 text-3xl font-bold'>Skills</h1>
+            <div className=' mx-auto flex space-x-4'>
+            {skills.map((skill, index) =>
+                <div className='text-lg text-center text-white'>
+                    <p>{skill.title}</p>
+                </div>
+            )}
+            </div>
         </div>
     )
 }
