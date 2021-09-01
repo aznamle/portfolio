9a9619e3221d9cb620c3a0698a56c32e83ae2f6f
aznamle <namle1998@hotmail.com>
2021-09-01 11:16:22 -0700

    api addition, project styling

--- components/Projects/Project.js
+++ components/Projects/Project.js
@@ -14,20 +14,20 @@ const Project = ({ project }) => {
     }
 
     return (
-        <div className='mx-auto max-w-3xl grid grid-cols-2 gap-4 py-12 text-white'>
+        <div className='mx-auto max-w-4xl grid grid-cols-2 gap-10 py-12 text-white'>
             {project.map(item => (
-                <div className='flex items-center text-center mx-auto justify-center border-b pb-4 border-opacity-50' key={item.id}>
+                <div className='flex items-center text-left mx-auto justify-center border-b pb-4 border-opacity-50' key={item.id}>
                     <div>
                         <h1 className='text-xl'>
                             {item.title}
                         </h1>
                         <p className='text-gray-400'>
-                            {item.description}
+                            {item.subtitle}
                         </p>
                     </div>
                     <div>
                         <button 
-                            className='bg-gray-700 border-white transition ease-in-out duration-300 hover:bg-gray-800 py-2 px-4 rounded-3xl ml-12' 
+                            className='bg-gray-700 transition ease-in-out duration-300 hover:bg-gray-800 py-2 px-5 rounded-3xl ml-12' 
                             onClick={() => openProject(item)}
                         >
                             View
