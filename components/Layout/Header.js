023b0b941e51e341c310f57bfb27617a69c0753a
aznamle <namle1998@hotmail.com>
2021-09-07 16:25:25 -0700

    small header change

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -15,41 +15,41 @@ const Header = () => {
                 <a className="hover:text-gray-500 dark:hover:text-gray-700 transition duration-300">Skills</a>
                 <a className="hover:text-gray-500 dark:hover:text-gray-700 transition duration-300">Projects</a>
                 <a className="hover:text-gray-500 dark:hover:text-gray-700 transition duration-300">Experience</a>
-              {colorTheme === 'light' ? (
-              <svg
-                className="w-6 h-6 transition duration-300"
-                onClick={() => setTheme('light')}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                cursor='pointer'
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
-                />
-              </svg>
-            ) : (
-              <svg
-                className="w-6 h-6 transition duration-300"
-                onClick={() => setTheme('dark')}
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                cursor='pointer'
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
-                />
-              </svg>
-            )}
+                {colorTheme === 'light' ? (
+                  <svg
+                    className="w-6 h-6 transition duration-300"
+                    onClick={() => setTheme('light')}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    cursor='pointer'
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                    />
+                  </svg>
+                ) : (
+                  <svg
+                    className="w-6 h-6 transition duration-300"
+                    onClick={() => setTheme('dark')}
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                    cursor='pointer'
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={2}
+                      d="M9.663 17h4.673M12 3v1m6.364 1.636l-.707.707M21 12h-1M4 12H3m3.343-5.657l-.707-.707m2.828 9.9a5 5 0 117.072 0l-.548.547A3.374 3.374 0 0014 18.469V19a2 2 0 11-4 0v-.531c0-.895-.356-1.754-.988-2.386l-.548-.547z"
+                    />
+                  </svg>
+                )}
                 </nav>
             </div>
 
