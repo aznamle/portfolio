a67673ed489a10d5e256d9c2b761c67f3c3ee086
aznamle <namle1998@hotmail.com>
2021-10-11 10:39:58 -0700

    changed color scheme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,11 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class', // or 'media' or 'class'
   theme: {
+    colors: {
+      black: colors.black,
+      white: colors.white,
+      gray: colors.gray,
+    },
     extend: {},
   },
   variants: {
