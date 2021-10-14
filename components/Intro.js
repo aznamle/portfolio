a5ca62b29259f162d17c3f8f01b0768e62ca96dd
aznamle <namle1998@hotmail.com>
2021-10-14 11:35:40 -0700

    padding fix

--- components/Intro.js
+++ components/Intro.js
@@ -53,7 +53,7 @@ const Intro = ({ about }) => {
                 {socials.map((social,index) => (
                         <button key={index}
                             className='bg-gray-200 dark:bg-gray-700 transition ease-in-out duration-300 
-                                hover:bg-gray-400 dark:hover:bg-gray-800 py-2 px-5 rounded-3xl' 
+                                hover:bg-gray-400 dark:hover:bg-gray-800 py-2 px-4 md:px-5 rounded-3xl' 
                             >
                                 <a href={social.url} target="_blank" rel="noreferrer">
                                     <div className='flex space-x-2 items-center'>
