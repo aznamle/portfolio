4ff0f65a31288a8987ac6a5a45e8c99215686ac9
aznamle <namle1998@hotmail.com>
2021-11-18 23:47:00 -0800

    changed some apidata

--- pages/api/about.js
+++ pages/api/about.js
@@ -3,24 +3,10 @@ const about = [
         id: 1,
         name: "Hello, I'm Nam! 👋",
         image: "",
-        role: "Software Developer",
+        role: "Software Engineer",
         location: "Rancho Cucamonga, California",
-        description: "I am a front-end developer focusing on Javascript ES6+! My stack includes React, NextJS, Prisma and Prismic.\n\n In my free time, I am a gamer, sports fan, and anime fan!",
-        email : "namle1998@hotmail.com",
-        social: [
-            {
-                title: "Twitter",
-                url: "https://twitter.com/aznamle"
-            },
-            {
-                title: "LinkedIn",
-                url: "https://twitter.com/aznamle"
-            },
-            {
-                title: "GitHub",
-                url: "https://github.com/aznamle"
-            },
-        ],
+        description: "I am a front-end Software Engineer focusing on Javascript ES6+, React, NextJS, and Redux.\n\n In my free time, I am a gamer, sports fan, and anime fan!",
+        email : "namtle98@gmail.com",
     },
 ]
 
