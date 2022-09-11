d34577562bf846d6034cead7db57700ae72e9728
aznamle <namle1998@hotmail.com>
2022-09-11 03:47:47 -0700

    resume fix

--- components/Layout/Footer.js
+++ components/Layout/Footer.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
     return (
-        <div className='pt-24 pb-12 text-center text-gray-500'>
+        <div className='pt-96 text-center text-gray-500'>
             <p>Designed by Nam Le</p>
             <p>Built with NextJS & TailwindCSS</p>
         </div>
